a463fe0bfd4a0de425f55d0467c391e659f28c3e
tng121 <t.naga.11@gmail.com>
2016-02-26 22:37:56 +0900

    復元中

---
+++ DeidentifyDPC/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace DeidentifyDPC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(9, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ファイル追加";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(9, 45);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(467, 208);
+            this.listBox1.TabIndex = 6;
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBox1_DragEnter);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(234, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(51, 29);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "全クリア";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(360, 10);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(73, 29);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "匿名化実行";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(291, 10);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(63, 29);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "タイプ指定";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(439, 10);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(38, 29);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "設定";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(88, 11);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(66, 28);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "選択反転";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(160, 11);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(68, 28);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "選択削除";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(9, 259);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(276, 21);
+            this.progressBar1.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(291, 260);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(184, 19);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.Text = "0 ファイル匿名化処理可";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 292);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "DeidentifyDPC - DPCテキスト匿名化ツール (ver.1.0)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ UnitTestDeidentifyDPC/UnitTest1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DeidentifyDPC;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTestDeidentifyDPC
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Transformation tr;
+
+            tr = new LeftRotateTransformation(3);
+            Assert.AreEqual((ulong)4567890123, tr.transform(1234567890));
+
+            tr = new LinearTransformation(11, 3);
+            Assert.AreEqual(3580246793, tr.transform(1234567890));
+
+            Assert.IsTrue(ModularExponentiateTransformation.primeFactors(60).SequenceEqual(new List<ulong> { 2, 2, 3, 5 }));
+
+            Assert.IsTrue(ModularExponentiateTransformation.primeFactors(1).SequenceEqual(new List<ulong> ()));
+
+            Assert.IsTrue(ModularExponentiateTransformation.primeFactors(15966421).SequenceEqual(new List<ulong> { 15966421 }));
+
+            Assert.IsNotNull(new ModularExponentiateTransformation(2, 2147483543, 269, 15966421, 5, 282013133, 17, 17, 17));
+
+            Assert.IsNotNull(new LinearTransformation());
+            Assert.IsNotNull(new LeftRotateTransformation());
+
+            tr = Transformation.StringConstrucor("{lr:3}");
+            Assert.AreEqual((ulong)4567890123, tr.transform(1234567890));
+
+            tr = Transformation.StringConstrucor("{ln:b,3}");
+            Assert.AreEqual(3580246793, tr.transform(1234567890));
+
+            tr = Transformation.StringConstrucor("{me:2,1___-n,4d,YW3l,5,gPOTd,h,h,h}");
+            Assert.IsNotNull(tr);
+            Console.WriteLine("2147483543.To64 => " + Transformation.To64(2147483543));
+            Console.WriteLine("269.To64 => " + Transformation.To64(269));
+            Console.WriteLine("15966421.To64 => " + Transformation.To64(15966421));
+            Console.WriteLine("282013133.To64 => " + Transformation.To64(282013133));
+            Console.WriteLine("17.To64 => " + Transformation.To64(17));
+
+            tr = new ModularExponentiateTransformation();
+            Console.WriteLine(tr.ToString());
+            Console.WriteLine("me(1234567890) => " + tr.transform(1234567890).ToString());
+            Assert.IsNotNull(tr);
+
+            IdEncryption enc = IdEncryption.StringConstructor("{ln:b,3}{lr:3}");
+            Assert.AreEqual((ulong)246793358, enc.encrypt(1234567890));
+
+            Assert.AreEqual("op" , Transformation.To64(24*64+25));
+
+            Assert.AreEqual((ulong)24 * 64 + 26, Transformation.From64("oq"));
+
+            bool thrown = false;
+            try
+            {
+                Transformation.From64("あああ");
+            }
+            catch (ArgumentException)
+            {
+                thrown = true;
+            }
+            Assert.AreEqual(true, thrown);
+
+            enc = new IdEncryption(new List<Transformation>() { new LinearTransformation(79565413, 1974568793), new LeftRotateTransformation(5) });
+
+            Console.WriteLine("enc2: " + enc.ToString());
+            Assert.IsNotNull(enc);
+
+            BirthDateModifier bdmod = new BirthDateTo0101();
+            Assert.AreEqual("20150101", bdmod.modify("20151201",null));
+            Assert.AreEqual("20160101", bdmod.modify("20167890",null));
+
+            bdmod = BirthDateModifier.TypedConstructor(0); //AdmDate
+            Assert.AreEqual("19801201", bdmod.modify("19800503", "20151201"));
+            Assert.AreEqual("19820503", bdmod.modify("19811201", "20150503"));
+            Assert.AreEqual(getAge(19800503, 20151201), getAge(19801201, 20151201));
+            Assert.AreEqual(getAge(19811201, 20150503), getAge(19820503, 20150503));
+            Assert.AreEqual("19811201", bdmod.modify("19811201", "20151201"));
+
+            PostalCodeModifier pcmod = new PostalCodeTo0000000();
+            Assert.AreEqual("0000000", pcmod.modify("1234567"));
+            Assert.AreEqual("0000000", pcmod.modify("9999999"));
+
+        }
+
+
+        private int getAge(int birthdate, int today)
+        {
+            return (today - birthdate) / 10000;
+        }
+
+    }
+}
